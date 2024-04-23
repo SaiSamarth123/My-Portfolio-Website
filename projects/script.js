8413fdc36cc8f2a18655d487d401e5cb497bb4af
SaiSamarth123 <sai003sam@gmail.com>
2024-04-22 20:25:41 -0500

    added projects

--- projects/script.js
+++ projects/script.js
@@ -39,67 +39,57 @@ function getProjects() {
       return data;
     });
 }
-
 function showProjects(projects) {
   let projectsContainer = document.querySelector(".work .box-container");
   let projectsHTML = "";
   projects.forEach((project) => {
+    // Join categories to create a class string
+    const categoryClasses = project.category
+      .join(" ")
+      .toLowerCase()
+      .replace(/, /g, " ");
+
     projectsHTML += `
-        <div class="grid-item ${project.category}">
+        <div class="grid-item ${categoryClasses}">
         <div class="box tilt" style="width: 380px; margin: 1rem">
-      <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="project" /> 
-      <div class="content">
-        <div class="tag">
-        <h3>${project.name}</h3>
-        </div>
-        <div class="desc">
-          <p>${project.desc}</p>
-          <div class="btns"> 
-          <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
-            <a href="${project.links.code}" class="btn" target="_blank">Code <i class="fas fa-code"></i></a>
+          <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="${project.name}" /> 
+          <div class="content">
+            <div class="tag">
+              <h3>${project.name}</h3>
+            </div>
+            <div class="desc">
+              <p>${project.desc}</p>
+              <div class="btns">
+                <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
+                <a href="${project.links.code}" class="btn" target="_blank"><i class="fas fa-code"></i> Code</a>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
-    </div>`;
+      </div>`;
   });
   projectsContainer.innerHTML = projectsHTML;
 
-  // vanilla tilt.js
-  // VanillaTilt.init(document.querySelectorAll(".tilt"), {
-  //     max: 20,
-  // });
-  // // vanilla tilt.js
-  //             <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
-
-  // /* ===== SCROLL REVEAL ANIMATION ===== */
-  // const srtop = ScrollReveal({
-  //     origin: 'bottom',
-  //     distance: '80px',
-  //     duration: 1000,
-  //     reset: true
-  // });
-
-  // /* SCROLL PROJECTS */
-  // srtop.reveal('.work .box', { interval: 200 });
-
-  // isotope filter products
+  // Isotope needs to reinitialize after content update
   var $grid = $(".box-container").isotope({
     itemSelector: ".grid-item",
     layoutMode: "fitRows",
-    masonry: {
-      columnWidth: 200,
-    },
   });
 
-  // filter items on button click
-  $(".button-group").on("click", "button", function () {
-    $(".button-group").find(".is-checked").removeClass("is-checked");
-    $(this).addClass("is-checked");
+  // Refresh Isotope layout after new projects are loaded
+  $grid.isotope("reloadItems").isotope();
+
+  // Filter items when filter button is clicked
+  $(".filter-btn").on("click", function () {
     var filterValue = $(this).attr("data-filter");
     $grid.isotope({ filter: filterValue });
   });
 }
+
+// Fetch and display projects on page load
+document.addEventListener("DOMContentLoaded", function () {
+  getProjects().then((data) => showProjects(data));
+});
 
 getProjects().then((data) => {
   showProjects(data);
