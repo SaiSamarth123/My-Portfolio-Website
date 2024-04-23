45c1c15ddce4de1480a51c7f5f81e6d1dd8b0810
SaiSamarth123 <sai003sam@gmail.com>
2024-04-22 23:18:31 -0500

    added projects

--- projects/script.js
+++ projects/script.js
@@ -40,48 +40,121 @@ function getProjects() {
     });
 }
 
+// function showProjects(projects) {
+//   let projectsContainer = document.querySelector(".work .box-container");
+//   let projectsHTML = "";
+//   projects.forEach((project) => {
+//     projectsHTML += `
+//         <div class="grid-item ${project.category}">
+//         <div class="box tilt" style="width: 380px; margin: 1rem">
+//       <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="project" />
+//       <div class="content">
+//         <div class="tag">
+//         <h3>${project.name}</h3>
+//         </div>
+//         <div class="desc">
+//           <p>${project.desc}</p>
+//           <div class="btns">
+//           <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
+//             <a href="${project.links.code}" class="btn" target="_blank">Code <i class="fas fa-code"></i></a>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//     </div>`;
+//   });
+//   projectsContainer.innerHTML = projectsHTML;
+
+//   // vanilla tilt.js
+//   // VanillaTilt.init(document.querySelectorAll(".tilt"), {
+//   //     max: 20,
+//   // });
+//   // // vanilla tilt.js
+//   //             <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
+
+//   // /* ===== SCROLL REVEAL ANIMATION ===== */
+//   // const srtop = ScrollReveal({
+//   //     origin: 'bottom',
+//   //     distance: '80px',
+//   //     duration: 1000,
+//   //     reset: true
+//   // });
+
+//   // /* SCROLL PROJECTS */
+//   // srtop.reveal('.work .box', { interval: 200 });
+
+//   // isotope filter products
+//   var $grid = $(".box-container").isotope({
+//     itemSelector: ".grid-item",
+//     layoutMode: "fitRows",
+//     masonry: {
+//       columnWidth: 200,
+//     },
+//   });
+
+//   // filter items on button click
+//   $(".button-group").on("click", "button", function () {
+//     $(".button-group").find(".is-checked").removeClass("is-checked");
+//     $(this).addClass("is-checked");
+//     var filterValue = $(this).attr("data-filter");
+//     $grid.isotope({ filter: filterValue });
+//   });
+// }
+
 function showProjects(projects) {
   let projectsContainer = document.querySelector(".work .box-container");
   let projectsHTML = "";
+  let projectsMap = new Map(); // Map to store unique projects
+
   projects.forEach((project) => {
-    projectsHTML += `
+    if (!projectsMap.has(project.name)) {
+      projectsMap.set(project.name, project);
+    }
+
+    if (Array.isArray(project.category)) {
+      project.category.forEach((category) => {
+        projectsHTML += `
+          <div class="grid-item ${category}">
+            <div class="box tilt" style="width: 380px; margin: 1rem">
+              <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="project" />
+              <div class="content">
+                <div class="tag">
+                  <h3>${project.name}</h3>
+                </div>
+                <div class="desc">
+                  <p>${project.desc}</p>
+                  <div class="btns">
+                    <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
+                    <a href="${project.links.code}" class="btn" target="_blank">Code <i class="fas fa-code"></i></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>`;
+      });
+    } else {
+      projectsHTML += `
         <div class="grid-item ${project.category}">
-        <div class="box tilt" style="width: 380px; margin: 1rem">
-      <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="project" /> 
-      <div class="content">
-        <div class="tag">
-        <h3>${project.name}</h3>
-        </div>
-        <div class="desc">
-          <p>${project.desc}</p>
-          <div class="btns"> 
-          <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
-            <a href="${project.links.code}" class="btn" target="_blank">Code <i class="fas fa-code"></i></a>
+          <div class="box tilt" style="width: 380px; margin: 1rem">
+            <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="project" />
+            <div class="content">
+              <div class="tag">
+                <h3>${project.name}</h3>
+              </div>
+              <div class="desc">
+                <p>${project.desc}</p>
+                <div class="btns">
+                  <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
+                  <a href="${project.links.code}" class="btn" target="_blank">Code <i class="fas fa-code"></i></a>
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-    </div>
-    </div>`;
+        </div>`;
+    }
   });
+
   projectsContainer.innerHTML = projectsHTML;
-
-  // vanilla tilt.js
-  // VanillaTilt.init(document.querySelectorAll(".tilt"), {
-  //     max: 20,
-  // });
-  // // vanilla tilt.js
-  //             <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
-
-  // /* ===== SCROLL REVEAL ANIMATION ===== */
-  // const srtop = ScrollReveal({
-  //     origin: 'bottom',
-  //     distance: '80px',
-  //     duration: 1000,
-  //     reset: true
-  // });
-
-  // /* SCROLL PROJECTS */
-  // srtop.reveal('.work .box', { interval: 200 });
 
   // isotope filter products
   var $grid = $(".box-container").isotope({
@@ -97,7 +170,16 @@ function showProjects(projects) {
     $(".button-group").find(".is-checked").removeClass("is-checked");
     $(this).addClass("is-checked");
     var filterValue = $(this).attr("data-filter");
-    $grid.isotope({ filter: filterValue });
+
+    $grid.isotope({
+      filter: function () {
+        if (filterValue === "*") {
+          return true; // Show all projects when "All Projects" filter is selected
+        } else {
+          return $(this).is(filterValue) || $(this).hasClass(filterValue);
+        }
+      },
+    });
   });
 }
 
